1444723ebec0b75f53e5f26fc67bc872959d73cc
ProGlot003 <45171006+ProGlot003@users.noreply.github.com>
2019-02-28 18:15:20 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,204 @@
+import sqlite3
+
+
+class DB:
+    def __init__(self):
+        conn = sqlite3.connect('news.db', check_same_thread=False)
+        self.conn = conn
+
+    def get_connection(self):
+        return self.conn
+
+    def __del__(self):
+        self.conn.close()
+
+
+class UserModel:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             user_name VARCHAR(50),
+                             password_hash VARCHAR(128)
+                             )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, user_name, password_hash):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO users 
+                          (user_name, password_hash) 
+                          VALUES (?,?)''', (user_name, password_hash))
+        cursor.close()
+        self.connection.commit()
+
+    def get(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = ?", (str(user_id)))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        return rows
+
+    def exists(self, user_name, password_hash):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_name = ? AND password_hash = ?", (user_name, password_hash))
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False, None)
+
+
+class NewsModel:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS news 
+                                  (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                   title VARCHAR(100),
+                                   content VARCHAR(1000),
+                                   user_id INTEGER
+                                   )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, title, content, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO news 
+                            (title, content, user_id) 
+                            VALUES (?,?,?)''', (title, content, str(user_id)))
+        cursor.close()
+        self.connection.commit()
+
+    def get(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM news WHERE id = ?", (str(news_id)))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self, user_id=None):
+        cursor = self.connection.cursor()
+        if user_id:
+            cursor.execute("SELECT * FROM news WHERE user_id = " + (str(user_id)) + " ORDER BY id DESC")
+        else:
+            cursor.execute("SELECT * FROM news")
+        rows = cursor.fetchall()
+        return rows
+
+    def delete(self, news_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''DELETE FROM news WHERE id = ''' + str(news_id))
+        cursor.close()
+        self.connection.commit()
+
+
+db = DB()
+user_model = UserModel(db.get_connection())
+news_model = NewsModel(db.get_connection())
+user_model.init_table()
+news_model.init_table()
+
+from flask import Flask, redirect, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+    reg = SubmitField('Регистрация')
+
+
+class RegForm(FlaskForm):
+    username = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_test = PasswordField('Повторите пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    back = SubmitField('Назад')
+    reg = SubmitField('Зарегистрироваться')
+
+
+class AddNewsForm(FlaskForm):
+    title = StringField('Заголовок новости', validators=[DataRequired()])
+    content = TextAreaField('Текст новости', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+user_id = None
+user_status = False
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global user_id, user_status
+    form = LoginForm()
+    user_status, user_id = user_model.exists(form.username.data, form.password.data)
+    if form.submit.data:
+        if form.validate_on_submit() and user_status:
+            return redirect('/news')
+    elif form.reg.data:
+        return redirect('/register')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    global user_id, user_status
+    form = RegForm()
+    if form.back.data:
+        return redirect('/login')
+    elif form.reg.data:
+        if form.validate_on_submit():
+            user_model.insert(form.username.data, form.password.data)
+            return redirect('/login')
+    return render_template('register.html', title='Авторизация', form=form)
+
+
+@app.route('/index')
+@app.route('/news')
+def news():
+    if user_status:
+        news_list = news_model.get_all(user_id)
+        print(news_list)
+        return render_template('news.html', news=news_list)
+    else:
+        return redirect('/login')
+
+
+@app.route('/add_news', methods=['GET', 'POST'])
+def add_news():
+    if not user_status:
+        return redirect('/login')
+    form = AddNewsForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        news_model.insert(title, content, user_id)
+        return redirect("/index")
+    return render_template('add_news.html', title='Добавление новости', form=form, username=user_id)
+
+
+@app.route('/delete_news/<int:news_id>', methods=['GET'])
+def delete_news(news_id):
+    if not user_status:
+        return redirect('/login')
+    news_model.delete(news_id)
+    return redirect("/index")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
