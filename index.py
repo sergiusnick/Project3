4fae320af2c428a439fbfd5b6dd7318f713534b5
ProGlot003 <45171006+ProGlot003@users.noreply.github.com>
2019-03-11 17:24:44 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -28,7 +28,8 @@ class UserModel:
                              user_name VARCHAR(50),
                              user_surname VARCHAR(50),
                              user_email VARCHAR(50),
-                             password_hash VARCHAR(128)
+                             password_hash VARCHAR(128),
+                             user_type INTEGER
                              )''')
         cursor.close()
         self.connection.commit()
@@ -98,11 +99,93 @@ class NewsModel:
         rows = cursor.fetchall()
         return rows
 
+    def get_feed(self, user_id):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            f'''SELECT * FROM news WHERE user_id in (SELECT follow_id from feed WHERE user_id =  {user_id} ) ORDER BY id DESC''')
+        rows = cursor.fetchall()
+        return rows
+
     def delete(self, news_id):
         cursor = self.connection.cursor()
         cursor.execute(f'''DELETE FROM news WHERE id = {news_id}''')
         cursor.close()
         self.connection.commit()
+
+
+class Message:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS messages 
+                                  (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                   from_user INTEGER,
+                                   to_user INTEGER,
+                                   content VARCHAR(2500)
+                                   )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, from_user, to_user, content):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO messages 
+                            (from_user, to_user, content) 
+                            VALUES (?,?,?)''', (str(from_user), str(to_user), content))
+        cursor.close()
+        self.connection.commit()
+
+    def get_all(self, to_user, from_user=None):
+        cursor = self.connection.cursor()
+        if from_user is None:
+            cursor.execute(f'''SELECT * FROM messages WHERE to_user = {to_user} ORDER BY id DESC''')
+        else:
+            cursor.execute(
+                f'''SELECT * FROM messages WHERE to_user = {to_user} and from_user = {from_user}  ORDER BY id DESC''')
+        rows = cursor.fetchall()
+        return rows
+
+    def delete(self, id):
+        cursor = self.connection.cursor()
+        cursor.execute(f'''DELETE FROM messages WHERE id = {id}''')
+        cursor.close()
+        self.connection.commit()
+
+
+class Feed:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS feed 
+                                  (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                   user_id INTEGER,
+                                   follow_id INTEGER
+                                   )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, user_id, follow_id):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO feed 
+                            ( user_id, follow_id) 
+                            VALUES (?,?)''', (str(user_id), str(follow_id)))
+        cursor.close()
+        self.connection.commit()
+
+    def delete(self, user_id, follow_id):
+        cursor = self.connection.cursor()
+        cursor.execute(f'''DELETE FROM feed WHERE  user_id= {user_id} and follow_id = {follow_id}''')
+        cursor.close()
+        self.connection.commit()
+
+    def exists_feed(self, user_id, test_id):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM feed WHERE user_id = " + str(user_id) + " AND follow_id = " + str(test_id))
+        row = cursor.fetchone()
+        return True if row else False
 
 
 class LoginForm(FlaskForm):
@@ -133,15 +216,15 @@ class Profile(FlaskForm):
     name = StringField('Имя')
 
 
-class Message(FlaskForm):
-    ls = StringField('Сообщения')
-
-
 db = DB()
 user_model = UserModel(db.get_connection())
 news_model = NewsModel(db.get_connection())
+feed_model = Feed(db.get_connection())
+message_model = Message(db.get_connection())
 user_model.init_table()
 news_model.init_table()
+feed_model.init_table()
+message_model.init_table()
 app = Flask(__name__, )
 app.config['SECRET_KEY'] = '12345'
 user_id = None
@@ -195,11 +278,51 @@ def profile(user_id):
                            Name=name, Surname=surname)
 
 
-
-
 @app.route('/index')
 @app.route('/news')
 def news():
+    if user_status:
+        news_list = news_model.get_feed(user_id)
+        return render_template('news.html', news=news_list)
+    else:
+        return redirect('/login')
+
+
+@app.route('/users/')
+def users():
+    if user_status:
+        user_list = user_model.get_all()
+        subs= []
+        for item in user_list:
+            if feed_model.exists_feed(user_id, item[0]):
+                subs.append(True)
+            else:
+
+                subs.append(False)
+
+        return render_template('users.html', test = "<h2>test</h2>",users=zip(user_list,subs))
+    else:
+        return redirect('/login')
+
+
+
+@app.route('/subsribe/<int:follow_id>', methods=['GET'])
+def subsribe(follow_id):
+    if not user_status:
+        return redirect('/login')
+    feed_model.insert(user_id, follow_id)
+    return redirect("/users")
+
+
+@app.route('/unsubsribe/<int:follow_id>', methods=['GET'])
+def unsubsribe(follow_id):
+    if not user_status:
+        return redirect('/login')
+    feed_model.delete(user_id, follow_id)
+    return redirect("/users")
+
+@app.route('/messages')
+def messages():
     if user_status:
         news_list = news_model.get_all(user_id)
         return render_template('news.html', news=news_list)
